55d1ff5ed8d74137e611cf838f82f00c9bbcf71c
sirokurokumasan <72143123+sirokurokumasan@users.noreply.github.com>
2020-10-28 18:10:22 +0900

    Update scratch.js

--- mono/scratch.js
+++ mono/scratch.js
@@ -1,33 +1,286 @@
-class Math {
+class Utilities {
+
   constructor() {}
 
   getInfo() {
     return {
-      id: 'Blocks',
-      name: 'Blocks',
+      id: 'utilities',
+      name: 'Utlities',
+
+      color1: '#8BC34A',
+      color2: '#7CB342',
+      color3: '#689F38',
+
+      menuIconURI: icon,
+
       blocks: [
-          {
-          opcode: 'power', 
-          blockType: Scratch.BlockType.REPORTER,
-          text: '[X]の[N]乗',
+        {
+          opcode: 'isExactly',
+
+          blockType: Scratch.BlockType.BOOLEAN,
+
+          text: 'is [A] exactly [B]?',
           arguments: {
-            X: {
+            A: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'apple'
+            },
+            B: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'APPLE'
+            }
+          }
+        },
+        {
+          opcode: 'isLessOrEqual',
+
+          blockType: Scratch.BlockType.BOOLEAN,
+
+          text: '[A] <= [B]',
+          arguments: {
+            A: {
+              type: Scratch.ArgumentType.NUMBER
+            },
+            B: {
               type: Scratch.ArgumentType.NUMBER,
-              defaultValue: ''
-               },
-            N: {
+              defaultValue: 50
+            }
+          }
+        },
+        {
+          opcode: 'isMoreOrEqual',
+
+          blockType: Scratch.BlockType.BOOLEAN,
+
+          text: '[A] >= [B]',
+          arguments: {
+            A: {
+              type: Scratch.ArgumentType.NUMBER
+            },
+            B: {
               type: Scratch.ArgumentType.NUMBER,
-              defaultValue: ''
-               }
-             }
-           }
-        ]
-     }
+              defaultValue: 50
+            }
+          }
+        },
+        {
+          opcode: 'trueBlock',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'true'
+        },
+        {
+          opcode: 'falseBlock',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: 'false'
+        },
+        {
+          opcode: 'exponent',
+
+          blockType: Scratch.BlockType.REPORTER,
+
+          text: '[A] ^ [B]',
+          arguments: {
+            A: {
+              type: Scratch.ArgumentType.NUMBER
+            },
+            B: {
+              type: Scratch.ArgumentType.NUMBER
+            }
+          }
+        },
+        {
+          opcode: 'pi',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'pi'
+        },
+        {
+          opcode: 'ternaryOperator',
+
+          blockType: Scratch.BlockType.REPORTER,
+
+          text: 'if [A] then [B] else [C]',
+          arguments: {
+            A: {
+              type: Scratch.ArgumentType.BOOLEAN
+            },
+            B: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'banana'
+            },
+            C: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'apple'
+            }
+          }
+        },
+        {
+          opcode: 'letters',
+
+          blockType: Scratch.BlockType.REPORTER,
+
+          text: 'letters [START] to [END] of [STRING]',
+          arguments: {
+            START: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 5
+            },
+            END: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 7
+            },
+            STRING: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'red apple'
+            }
+          }
+        },
+        {
+          opcode: 'currentMillisecond',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'current millisecond'
+        },
+        {
+          opcode: 'fetchFrom',
+
+          blockType: Scratch.BlockType.REPORTER,
+
+          text: 'get content from [URL]',
+          arguments: {
+            URL: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'https://translate-service.scratch.mit.edu/translate?language=eo&text=hello'
+            }
+          }
+        },
+        {
+          opcode: 'parseJSON',
+
+          blockType: Scratch.BlockType.REPORTER,
+
+          text: '[PATH] of [JSON_STRING]',
+          arguments: {
+            PATH: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'fruit/apples'
+            },
+            JSON_STRING: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: '{"fruit": {"apples": 2, "bananas": 3}, "total_fruit": 5}'
+            }
+          }
+        },
+        {
+          opcode: 'stringToBoolean',
+
+          blockType: Scratch.BlockType.BOOLEAN,
+
+          text: '[STRING]',
+          arguments: {
+            STRING: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'true'
+            }
+          }
+        },
+        {
+          opcode: 'regexReplace',
+
+          blockType: Scratch.BlockType.REPORTER,
+
+          text: 'replace [STRING] using the rule [REGEX] with [NEWSTRING]',
+          arguments: {
+            STRING: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'バナナはすばらしい。バナナが好きだ。'
+            },
+            REGEX: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'バナナ'
+            },
+            NEWSTRING: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'りんご'
+            }
+          }
+        }
+      ]
+    }
   }
 
-  power(args) {
-    return Math.pow(args.X, args.N);
+  isExactly({A, B}) {
+    return A === B;
   }
+
+  isLessOrEqual({A, B}) {
+    return A ≦ B;
+  }
+
+  isMoreOrEqual({A, B}) {
+    return A ≧ B;
+  }
+
+  trueBlock() {
+    return true;
+  }
+
+  falseBlock() {
+    return false;
+  }
+
+  exponent({A, B}) {
+    return Math.pow(A, B);
+  }
+
+  pi() {
+    return Math.PI;
+  }
+
+  ternaryOperator({A, B, C}) {
+    return A ? B : C;
+  }
+
+  letters({STRING, START, END}) {
+    return STRING.slice(Math.max(1, START) - 1, Math.min(STRING.length, END));
+  }
+
+  currentMillisecond() {
+    return Date.now() % 1000;
+  }
+
+  fetchFrom({URL}) {
+    return fetch(URL).then(res => res.text())
+      .catch(err => '');
+  }
+
+  parseJSON({PATH, JSON_STRING}) {
+    try {
+      const path = PATH.toString().split('/').map(prop => decodeURIComponent(prop));
+      if (path[0] === '') path.splice(0, 1);
+      if (path[path.length - 1] === '') path.splice(-1, 1);
+      let json;
+      try {
+        json = JSON.parse(' ' + JSON_STRING);
+      } catch (e) {
+        return e.message;
+      }
+      path.forEach(prop => json = json[prop]);
+      if (json === null) return 'null';
+      else if (json === undefined) return '';
+      else if (typeof json === 'object') return JSON.stringify(json);
+      else return json.toString();
+    } catch (err) {
+      return '';
+    }
+  }
+
+  stringToBoolean({STRING}) {
+    return STRING;
+  }
+
+  regexReplace({STRING, REGEX, NEWSTRING}) {
+    return STRING.toString().replace(new RegExp(REGEX, 'gi'), NEWSTRING);
+  }
+
 }
 
-Scratch.extensions.register(new Math());
+Scratch.extensions.register(new Utilities());
